24cc389e8edaa4606f08f526f03dbcb6d8dbf0b6
Jubril O <jubrylola@gmail.com>
2025-08-02 13:04:35 +0100

    Initial Commit

---
+++ ImageCropper/ImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropperApp.swift
+//  ImageCropper
+//
+//  Created by Jubril Olambiwonnu on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
